38d31b04f58beb664781272343412f0d3841646d
nenen2ya <nenen2ya@khu.ac.kr>
2025-10-27 17:52:30 +0900

    라우팅 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Mypage from "./pages/Mypage";
-import Signup from "./pages/Signup";
-import Auth from "./pages/Auth";
 import Report from "./pages/Report";
 import SubReport from "./pages/SubReport";
-import Favorites from "./pages/Favorites";
-import Insurance from "./pages/Insurance";
 import Diagnose from "./pages/Diagnose";
 import TopBanner from "./components/TopBanner";
 import Myinsur from "./pages/Myinsur";
@@ -26,12 +22,8 @@ const App: React.FC = () => {
         <Route path="/home" element={<Home />} />
         <Route path="/home/:user_id" element={<Home />} />
         <Route path="/mypage/:user_id" element={<Mypage />} />
-        <Route path="/auth" element={<Auth />} />
         <Route path="/report" element={<Report />} />
         <Route path="/subreport" element={<SubReport />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/favorites" element={<Favorites />} />
-        <Route path="/insurance" element={<Insurance />} />
         <Route path="/diagnose" element={<Diagnose />} />
         <Route path="/myinsur" element={<Myinsur />} />
         <Route path="/inmypocket" element={<Inmypocket />} />
